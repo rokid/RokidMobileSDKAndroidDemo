9ad4caab24dde8e8afe8d81b2408e23d5d33d1f9
xupan.shi <xupan.shi@rokid.com>
2018-02-02 20:05:21 +0800

    新增 Skill 接口 闹钟 提醒

---
+++ RokidSDKDemokotlin/app/src/main/kotlin/com/rokid/mobile/sdk/demo/HA.java
@@ -0,0 +1,18 @@
+package com.rokid.mobile.sdk.demo;
+
+import com.rokid.mobile.sdk.RokidMobileSDK;
+
+/**
+ * Description: TODO
+ * Author: Shper
+ * Version: V0.1 2018/2/2
+ */
+public class HA {
+
+    public void xxx() {
+
+        RokidMobileSDK.skill.remind.getRemindList();
+
+    }
+
+}
