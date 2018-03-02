55f1583faad2c29740e499386b25093ba96c856b
yongtao ye <yongtao.ye@rokid.com>
2018-03-02 10:43:39 +0800

    修改 设备解绑UI

---
+++ RokidSDKDemoKotlin/app/src/main/kotlin/com/rokid/mobile/sdk/demo/base/item/DeviceItem.kt
@@ -0,0 +1,44 @@
+package com.rokid.mobile.sdk.demo.base.adapter.item
+
+import android.view.View
+import com.rokid.mobile.lib.entity.bean.device.RKDevice
+import com.rokid.mobile.sdk.demo.R
+import com.rokid.mobile.ui.recyclerview.item.BaseItem
+import com.rokid.mobile.ui.recyclerview.item.BaseViewHolder
+import kotlinx.android.synthetic.main.device_item_list.view.*
+
+typealias Unbind = () -> Unit
+
+class DeviceItem(data: RKDevice, var unbind: Unbind) : BaseItem<RKDevice>(data) {
+
+    val DeviceItem_ITEM_TYPE = 0
+
+    override fun getViewType(): Int = DeviceItem_ITEM_TYPE
+
+    override fun getLayoutId(viewType: Int): Int = R.layout.device_item_list
+
+    override fun onReleaseViews(holder: BaseViewHolder?, sectionKey: Int, sectionViewPosition: Int) {
+        holder!!.itemView.apply {
+            fragment_device_id.text = ""
+            fragment_device_id.visibility = View.GONE
+            fragment_device_unbind.visibility = View.GONE
+        }
+    }
+
+    override fun onSetViewsData(holder: BaseViewHolder?, sectionKey: Int, sectionViewPosition: Int) {
+        holder!!.itemView.apply {
+            fragment_device_id.text = data.rokiId
+            fragment_device_id.visibility = View.VISIBLE
+            fragment_device_unbind.visibility = View.VISIBLE
+
+            setOnClick(fragment_device_unbind)
+        }
+    }
+
+    private fun setOnClick(view: View) {
+        view.visibility = View.VISIBLE
+        view.setOnClickListener {
+            unbind()
+        }
+    }
+}
--- RokidSDKDemoKotlin/app/src/main/kotlin/com/rokid/mobile/sdk/demo/device/DeviceListFragment.kt
+++ RokidSDKDemoKotlin/app/src/main/kotlin/com/rokid/mobile/sdk/demo/device/DeviceListFragment.kt
@@ -8,27 +8,41 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.Toast
+import com.rokid.mobile.lib.base.util.CollectionUtils
 import com.rokid.mobile.lib.base.util.Logger
+import com.rokid.mobile.lib.entity.bean.device.RKDevice
 import com.rokid.mobile.lib.entity.event.device.EventDeviceSysUpdate
+import com.rokid.mobile.lib.xbase.device.callback.IGetDeviceListCallback
+import com.rokid.mobile.lib.xbase.device.callback.IUnbindDeviceCallback
+import com.rokid.mobile.sdk.RokidMobileSDK
 import com.rokid.mobile.sdk.demo.R
 import com.rokid.mobile.sdk.demo.base.BaseFragment
 import com.rokid.mobile.sdk.demo.base.TypeConstants
 import com.rokid.mobile.sdk.demo.base.adapter.data.ActionData
 import com.rokid.mobile.sdk.demo.base.adapter.item.ActionItem
+import com.rokid.mobile.sdk.demo.base.adapter.item.DeviceItem
 import com.rokid.mobile.sdk.demo.base.adapter.item.UnbindItem
 import com.rokid.mobile.ui.recyclerview.adapter.BaseRVAdapter
 import com.rokid.mobile.ui.recyclerview.item.BaseItem
+import kotlinx.android.synthetic.main.device_fragment_list.*
+import kotlinx.android.synthetic.main.device_fragment_list.view.*
 import org.greenrobot.eventbus.EventBus
 import org.greenrobot.eventbus.Subscribe
 import java.util.*
+import kotlin.collections.ArrayList
 
 /**
  * Created by wangshuwen on 2017/12/4.
  */
 class DeviceListFragment : BaseFragment() {
 
+    lateinit var deviceBtn: Button
+    lateinit var progressBar: ProgressBar
     lateinit var mRecycler: RecyclerView
-    lateinit var mAdapter: BaseRVAdapter<BaseItem<Any>>
+    lateinit var mAdapter: BaseRVAdapter<DeviceItem>
 
     override fun layoutId(): Int = R.layout.device_fragment_list
 
@@ -37,35 +51,66 @@ class DeviceListFragment : BaseFragment() {
             EventBus.getDefault().register(this)
         }
 
-        mRecycler = rootView!!.findViewById(R.id.fragment_common_rv)
+        deviceBtn = rootView!!.fragment_device_list_btn
+        progressBar = rootView!!.fragment_device_list_pb
+        mRecycler = rootView!!.fragment_device_list_rv
 
         mRecycler.apply {
             layoutManager = LinearLayoutManager(activity as Context?)
             mAdapter = BaseRVAdapter()
             adapter = mAdapter
         }
-
-        mAdapter.setItemViewList(getItemList())
     }
 
     override fun initListeners() {
+        deviceBtn.setOnClickListener {
+            getDeviceItemList()
+        }
+
+        mAdapter.setOnItemViewClickListener { deviceItem, sectionKey, sectionItemPosition ->
+            Logger.i("onItemClick position=" + sectionItemPosition + " deviceId=" + deviceItem.data.rokiId)
+            toast(deviceItem.data.toString())
+        }
     }
 
-    fun getItemList(): List<BaseItem<Any>> {
-
-        val itemList = ArrayList<BaseItem<Any>>()
-
-        val actionList = listOf(
-                ActionData("获取设备列表", TypeConstants.GET_DEVICE_LIST),
-                ActionData("解绑设备", TypeConstants.UNBIND))
-
-        actionList.forEach {
-            when (it.type) {
-                TypeConstants.UNBIND -> itemList.add(UnbindItem(it) as BaseItem<Any>)
-                else -> itemList.add(ActionItem(it) as BaseItem<Any>)
+    fun getDeviceItemList() {
+        progressBar.visibility = View.VISIBLE
+        mRecycler.visibility = View.GONE
+        mAdapter.clearAllItemView()
+        RokidMobileSDK.device.getDeviceList(object : IGetDeviceListCallback {
+            override fun onGetDeviceListFailed(p0: String?, p1: String?) {
+                toast("获取设备列表失败 errorCode=" + (p0 ?: "") + "errorMsg= " + (p1 ?: ""))
             }
-        }
-        return itemList
+
+            override fun onGetDeviceListSucceed(deviceList: MutableList<RKDevice>?) {
+                progressBar.visibility = View.GONE
+                if (CollectionUtils.isEmpty(deviceList)) {
+                    toast("设备列表为空")
+                    mRecycler.visibility = View.GONE
+                    return
+                }
+
+                val deviceItemList = ArrayList<DeviceItem>()
+                deviceList!!.forEachIndexed { index, rkDevice ->
+                    deviceItemList.add(DeviceItem(rkDevice, {
+                        RokidMobileSDK.device.unbindDevice(rkDevice.rokiId!!, object : IUnbindDeviceCallback {
+                            override fun onUnbindDeviceSucceed() {
+                                toast("解绑设备成功")
+                                mAdapter.removeItemView(index)
+                                mAdapter.notifyDataSetChanged()
+                            }
+
+                            override fun onUnbindDeviceFailed(p0: String?, p1: String?) {
+                                toast("解绑设备失败 errorCode=" + (p0 ?: "") + "errorMsg= " + (p1 ?: ""))
+                            }
+                        })
+                    }))
+                }
+
+                mAdapter.setItemViewList(deviceItemList)
+                mRecycler.visibility = View.VISIBLE
+            }
+        })
     }
 
     override fun onDestroy() {
